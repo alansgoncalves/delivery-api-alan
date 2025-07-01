379c1faf5271f1207c95f2fdcb2914e8ba402d22
Alan Goncalves <alansg.logistics@gmail.com>
2025-07-01 22:27:31 +0000

    feat: criar controller Hello

--- src/main/java/com/deliverytech/delivery/HelloController.java
+++ src/main/java/com/deliverytech/delivery/HelloController.java
@@ -3,10 +3,10 @@ package com.deliverytech.delivery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-    @GetMapping("/hello")  // Path explícito
-    public String hello() {
-        return "Olá, mundo!";
-    }
+@RestController 
+public  class HelloController {
+	@GetMapping("")
+	public String hello() {
+		return "Olá, mundo!";
+	}
 }

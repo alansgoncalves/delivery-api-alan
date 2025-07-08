552a4c8e6221ae250af39145ad0d2ae2870d9733
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-07-07 21:34:10 -0300

    feat: implementa model para Cliente e Pedido

---
+++ src/main/java/com/deliverytech/delivery/model/Cliente.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private Boolean ativo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
+}

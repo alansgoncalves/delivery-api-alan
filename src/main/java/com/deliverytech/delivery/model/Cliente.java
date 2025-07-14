9f183372721c4fc9775088904c23750de357ff9e
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-07-14 08:20:09 -0300

    feat: cria arquivos dto

--- src/main/java/com/deliverytech/delivery/model/Cliente.java
+++ src/main/java/com/deliverytech/delivery/model/Cliente.java
@@ -8,15 +8,15 @@ import java.util.List;
 @Data
 
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    private String email;
-    private String telefone;
-    private String endereco;
-    private Boolean ativo;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String email;
+  private String telefone;
+  private String endereco;
+  private Boolean ativo;
 
-    @OneToMany(mappedBy = "cliente")
-    private List<Pedido> pedidos;
+  @OneToMany(mappedBy = "cliente")
+  private List<Pedido> pedidos;
 }

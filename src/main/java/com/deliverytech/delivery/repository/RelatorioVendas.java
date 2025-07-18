3a3339e2efdbdf5ff1621aa46612f5f053ee5f3a
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-07-18 19:33:28 -0300

    feat: cria RelatorioVendas em repository

---
+++ src/main/java/com/deliverytech/delivery/repository/RelatorioVendas.java
@@ -0,0 +1,11 @@
+package com.deliverytech.delivery.repository;
+
+import java.math.BigDecimal;
+
+public interface RelatorioVendas {
+  String getNomeRestaurante();
+
+  BigDecimal getTotalVendas();
+
+  Long getQuantidadePedidos();
+}

9e40bfe77b3be22f75e33bd43f7872c24fbf3ea0
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-07-28 14:04:39 -0300

    fix: ajusta arquivo application.properties

---
+++ src/test/java/com/deliverytech/delivery_api/DeliveryApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.deliverytech.delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.deliverytech.delivery.DeliveryApiApplication;
+
+@SpringBootTest(classes = DeliveryApiApplication.class)
+class DeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
